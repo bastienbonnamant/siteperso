4dd66247cffc197d6eb29eba5b711022e596cacb
bastienbonnamant <bastien.bonnamant@gmail.com>
2021-05-04 21:50:04 +0200

    ajout css services, footer, card blog, admin add article

--- src/Model/BlogManager.php
+++ src/Model/BlogManager.php
@@ -1,8 +1,6 @@
 <?php
 
-
 namespace App\Model;
-
 
 class BlogManager extends AbstractManager
 {
@@ -10,7 +8,8 @@ class BlogManager extends AbstractManager
 
     public function insert(array $blog): int
     {
-        $statement = $this->pdo->prepare(" INSERT INTO " . self::TABLE . " (`title`, `date`, `name`, `text`) VALUES (:title,:date,:name,:text)");
+        $statement = $this->pdo->prepare(" INSERT INTO " . self::TABLE . " (`title`, `date`, `name`, `text`) 
+        VALUES (:title,:date,:name,:text)");
         $statement->bindValue('title', $blog['title'], \PDO::PARAM_STR);
         $statement->bindValue('date', $blog['date'], \PDO::PARAM_INT);
         $statement->bindValue('name', $blog['name'], \PDO::PARAM_STR);
@@ -22,15 +21,14 @@ class BlogManager extends AbstractManager
 
     public function update(array $blog): bool
     {
-        $statement = $this->pdo->prepare("UPDATE " . self::TABLE . " SET `title`=:title, `date`=:date, `name`=:name, `text`=:text WHERE id=:id");
+        $statement = $this->pdo->prepare("UPDATE " . self::TABLE . " SET `title`=:title, `date`=:date, 
+        `name`=:name, `text`=:text WHERE id=:id");
         $statement->bindValue('id', $blog['id'], \PDO::PARAM_INT);
         $statement->bindValue('title', $blog['title'], \PDO::PARAM_STR);
         $statement->bindValue('date', $blog['date'], \PDO::PARAM_INT);
         $statement->bindValue('name', $blog['name'], \PDO::PARAM_STR);
         $statement->bindValue('text', $blog['text'], \PDO::PARAM_STR);
 
-
         return $statement->execute();
     }
-
 }
